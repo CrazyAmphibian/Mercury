b7aceb78d908bb407591b5c77e7938bbe8396a76
CrazyAmphibian <68451232+CrazyAmphibian@users.noreply.github.com>
2026-07-27 21:37:00 -0700

    preparing for future, better error logging at least

--- src/mercury_error.cpp
+++ src/mercury_error.cpp
@@ -31,16 +31,19 @@ inline const char* get_type_string(uint8_t type) {
 }
 
 mercury_string* mercury_generate_error_string(mercury_state* M, const uint32_t errorcode, const mercury_int* data1, const mercury_int* data2, const mercury_int* data3) {
-	char* buffer = (char*)calloc(255,sizeof(char));
+	const unsigned int buffer_size=1000;
+	char* buffer = (char*)calloc(buffer_size,sizeof(char));
 	if (!buffer)return nullptr;
 	//char buffer[255] = {0};
 	int result = 0;
 
-	char header[255] = {0};
+	char header[buffer_size] = {0};
+	unsigned int used_space = 0;
+start:
+
 	if (M->bytecode.debug_info) {
-
+		
 		const char* fallback = "";
-
 		mercury_debug_token T = M->bytecode.debug_info[M->programcounter-1];
 		char* tchars=(char*)malloc(T.num_chars+1);
 		if (tchars) {
@@ -48,53 +51,64 @@ mercury_string* mercury_generate_error_string(mercury_state* M, const uint32_t e
 			tchars[T.num_chars] = '\0';
 		}
 #if defined(DEBUG) || defined(_DEBUG)
-		snprintf(header, 255, "line %zi col %zi instruction #%zi (%04X) at \"%s\"", T.line + 1, T.col + 1, M->programcounter-1, M->bytecode.instructions[M->programcounter-1], tchars ? tchars : fallback);
+		used_space+=snprintf(header + used_space, buffer_size- used_space, "line %zi col %zi instruction #%zi (%04X) at \"%s\"", T.line + 1, T.col + 1, M->programcounter-1, M->bytecode.instructions[M->programcounter-1], tchars ? tchars : fallback);
 #else
-		snprintf(header, 255, "line %zi col %zi at \"%s\"", T.line + 1, T.col + 1, tchars ? tchars : fallback);
+		used_space += snprintf(header + used_space, buffer_size - used_space, "line %zi col %zi at \"%s\"", T.line + 1, T.col + 1, tchars ? tchars : fallback);
 #endif
 	}
 	else {
-		snprintf(header, 255, "line ? col ? at ? - instruction #%zi (%04X)",M->programcounter-1,M->bytecode.instructions[M->programcounter-1]);
+		used_space += snprintf(header+ used_space, buffer_size - used_space, "line ? col ? at ? - instruction #%zi (%04X)",M->programcounter-1,M->bytecode.instructions[M->programcounter-1]);
+	}
+
+	M = M->parentstate;
+	if (M) {
+		if (used_space < buffer_size + 10) {
+			header[used_space++] = ' ';
+			header[used_space++] = 'i';
+			header[used_space++] = 'n';
+			header[used_space++] = '\n';
+		}
+		goto start;
 	}
 
 	switch (errorcode) {
 	case M_ERROR_NONE:
 		return nullptr;
 	case M_ERROR_ALLOCATION:
-		result=snprintf(buffer, 255, "%s: memory allocation error\n",header);
+		result=snprintf(buffer, buffer_size, "%s: memory allocation error\n",header);
 		return mercury_cstring_const_to_mstring(buffer,strlen(buffer));
 	case M_ERROR_WRONG_TYPE:
-		result = snprintf(buffer, 255, "%s: arg %zi wrong type. expected %s, got %s\n", header ,*data3 , get_type_string((uint8_t)*data2) , get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: arg %zi wrong type. expected %s, got %s\n", header ,*data3 , get_type_string((uint8_t)*data2) , get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_DIV_ZERO:
-		result = snprintf(buffer, 255, "%s: integer division by 0\n", header);
+		result = snprintf(buffer, buffer_size, "%s: integer division by 0\n", header);
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_INSTRUCTION_FAILIURE:
-		result = snprintf(buffer, 255, "%s: failiure to execute instruction\n", header);
+		result = snprintf(buffer, buffer_size, "%s: failiure to execute instruction\n", header);
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_CALL_NOT_FUNCTION:
-		result = snprintf(buffer, 255, "%s: attempt to call non-function value %s \n", header , get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: attempt to call non-function value %s \n", header , get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_INDEX_INVALID_TYPE:
-		result = snprintf(buffer, 255, "%s: attempt to index invalid variable type %s \n", header, get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: attempt to index invalid variable type %s \n", header, get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_NOT_ENOUGH_ARGS:
-		result = snprintf(buffer, 255, "%s: incorrect number of args. expected %zi, got %zi \n", header, *data2, *data1);
+		result = snprintf(buffer, buffer_size, "%s: incorrect number of args. expected %zi, got %zi \n", header, *data2, *data1);
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_CUSTOM_STRING:
-		result = snprintf(buffer, 255, "%s: %s \n", header, (char*)data1);
+		result = snprintf(buffer, buffer_size, "%s: %s \n", header, (char*)data1);
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_WRONG_TYPE_EXPECTS_ANY_NUMBER:
-		result = snprintf(buffer, 255, "%s: arg %zi wrong type. expected a number, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: arg %zi wrong type. expected a number, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_WRONG_TYPE_EXPECTS_ANY_FUNCTION:
-		result = snprintf(buffer, 255, "%s: arg %zi wrong type. expected any function type, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: arg %zi wrong type. expected any function type, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	case M_ERROR_WRONG_TYPE_EXPECTS_ANY_STORAGETYPE:
-		result = snprintf(buffer, 255, "%s: arg %zi wrong type. expected an array or table, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
+		result = snprintf(buffer, buffer_size, "%s: arg %zi wrong type. expected an array or table, got %s\n", header, *data2, get_type_string((uint8_t)*data1));
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	default:
-		result = snprintf(buffer, 255, "%s: unknown error\n",header);
+		result = snprintf(buffer, buffer_size, "%s: unknown error\n",header);
 		return mercury_cstring_const_to_mstring(buffer, strlen(buffer));
 	}
 }
