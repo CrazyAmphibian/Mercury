5e48b0f58cab7d290d648105545d71345d90dabb
CrazyAmphibian <68451232+CrazyAmphibian@users.noreply.github.com>
2026-09-19 11:14:24 -0700

    FINALLY fixed linux compiling (for real this time)

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir out
 mkdir out/linux_release
-g++ -O3 -fpermissive -pthread -DMERCURY_DYNAMIC_LIBRARY_COMPILE -shared -fPIC -o ./mercury.so src/mercury.cpp src/mercury_error.cpp src/mercury_bytecode.cpp src/mercury_compiler.cpp src/libs/mercury_lib_std.cpp src/libs/mercury_lib_math.cpp src/libs/mercury_lib_array.cpp src/libs/mercury_lib_string.cpp src/libs/mercury_lib_thread.cpp src/libs/mercury_lib_io.cpp src/libs/mercury_lib_os.cpp src/libs/mercury_lib_table.cpp src/libs/mercury_lib_debug.cpp
-g++ -O3 -fpermissive -pthread src/mercury_runtime.cpp -o out/linux_release/mercury ./mercury.so -Wl,-rpath=. -ldl
+g++ -O3 -fpermissive -pthread -DMERCURY_DYNAMIC_LIBRARY_COMPILE -shared -fPIC src/mercury.cpp src/mercury_error.cpp src/mercury_bytecode.cpp src/mercury_compiler.cpp src/libs/mercury_lib_std.cpp src/libs/mercury_lib_math.cpp src/libs/mercury_lib_array.cpp src/libs/mercury_lib_string.cpp src/libs/mercury_lib_thread.cpp src/libs/mercury_lib_io.cpp src/libs/mercury_lib_os.cpp src/libs/mercury_lib_table.cpp src/libs/mercury_lib_debug.cpp -o mercury.so
+g++ -O3 -fpermissive -pthread src/mercury_runtime.cpp mercury.so -Wl,-rpath,'$ORIGIN' -o mercury
 mv -f mercury.so out/linux_release/mercury.so
+mv -f mercury out/linux_release/mercury
